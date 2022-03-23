9126bae622066e5678ce8ede669ac1447b91e1ba
rivinda.22 <QZOHIG@yandex.ru>
2022-03-23 10:13:20 +0300

    1

--- src/main/java/com/dunga/telegram/App.java
+++ src/main/java/com/dunga/telegram/App.java
@@ -8,7 +8,6 @@ import org.telegram.telegrambots.ApiContextInitializer;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        // Здесь код написан по заветам
         // https://github.com/rubenlagus/TelegramBots/tree/master/telegrambots-spring-boot-starter
         ApiContextInitializer.init();
 
